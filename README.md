e8e005566a991cf4e9fbe75b3f584d51ea0d5d34
YOOJAESEOK <69152917+yoo94@users.noreply.github.com>
2025-04-22 10:13:33 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Welcome to my Expo starter 👋
+
+This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
+
+## Get started
+
+1. Install dependencies
+
+   ```bash
+   npm install
+   ```
+
+2. Start the app
+
+   ```bash
+    npx expo start
+   ```
+
+---
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "black",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+        }}
+      />
+    </Tabs>
+  );
+}
