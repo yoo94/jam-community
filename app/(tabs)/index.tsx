3a975d3962eedcaee74b9acf7ed591fd24d86758
YOOJAESEOK <69152917+yoo94@users.noreply.github.com>
2025-04-23 23:08:47 +0900

    feat:feed 마크업

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,14 @@
-import FeedItem from "@/components/FeedItem";
-import { SafeAreaView } from "react-native";
+import FeedList from "@/components/FeedList";
+import { colors } from "@/constants";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <FeedItem
-        post={{
-          id: 1,
-          userId: 1,
-          title: "더미제목",
-          description: "더미내용",
-          createdAt: "",
-          author: {
-            id: 1,
-            nickname: "더미이름",
-            imageUri: "",
-          },
-          imageUris: [],
-          likes: [],
-          hasVote: false,
-          voteCount: 1,
-          commentCount: 1,
-          viewCount: 1,
-        }}
-      />
+    <SafeAreaView style={styles.container}>
+      <FeedList />
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: colors.WHITE },
+});
---
+++ components/FeedList.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { FlatList } from "react-native";
+import FeedItem from "./FeedItem";
+import { colors } from "@/constants";
+
+const dummyData = [
+  {
+    id: 1,
+    userId: 1,
+    title: "더미제목",
+    description: "더미내용",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "더미이름",
+      imageUri: "",
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "더미제목",
+    description: "더미내용",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "더미이름",
+      imageUri: "",
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+];
+interface FeedListProps {}
+
+function FeedList({}: FeedListProps) {
+  return (
+    <FlatList
+      data={dummyData}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GREY_200,
+    gap: 12,
+  },
+});
+
+export default FeedList;
