23a59c116f26780f84e9496c46d95ae794e27e65
YOOJAESEOK <69152917+yoo94@users.noreply.github.com>
2025-04-24 23:33:50 +0900

    feat: 오타수정

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,17 +1,14 @@
 import FixedBottomCTA from "@/components/FixedBottomCTA";
-import CustomButton from "@/components/CustomButton";
-import InputFeild from "@/components/InputFeild";
-import { colors } from "@/constants";
+import InputField from "@/components/InputField";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 function LoginScreen() {
   return (
     <>
       <View style={styles.container}>
-        <InputFeild label={"이메일"} placeholder="이메일을 입력해주세요" />
-        <InputFeild label={"비밀번호"} placeholder="비밀번호를 입력해주세요" />
+        <InputField label={"이메일"} placeholder="이메일을 입력해주세요" />
+        <InputField label={"비밀번호"} placeholder="비밀번호를 입력해주세요" />
       </View>
       <FixedBottomCTA label="로그인하기" onPress={() => {}} />
     </>
