c4c8afc03335c244d2c08f60db5a3ad5fa4cec03
YOOJAESEOK <69152917+yoo94@users.noreply.github.com>
2025-06-08 22:32:27 +0900

    feat: 낙관적 업데이트

--- hooks/qureies/useLikePost.ts
+++ hooks/qureies/useLikePost.ts
@@ -1,21 +1,62 @@
 import { likePost } from "@/api/post";
 import queryClient from "@/api/queryClient";
 import { queryKeys } from "@/constants";
+import { Post, Profile } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
 function useLikePost() {
   return useMutation({
     mutationFn: likePost,
-    onSuccess: (postId) => {
-      queryClient.invalidateQueries({
-        queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
+    onMutate: async (postId: number) => {
+      await queryClient.cancelQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POSTS, postId],
       });
+
+      const user = queryClient.getQueryData<Profile>([
+        queryKeys.AUTH,
+        queryKeys.USER_INFO,
+      ]);
+      const userId = user?.id;
+
+      if (userId !== undefined) {
+        const previousPosts = queryClient.getQueryData<Post>([
+          queryKeys.POST,
+          queryKeys.GET_POSTS,
+          postId,
+        ]);
+
+        if (previousPosts) {
+          const newPost = { ...previousPosts };
+          const likeIndex = previousPosts.likes.findIndex(
+            (like) => like.userId === userId
+          );
+
+          likeIndex >= 0
+            ? newPost.likes?.splice(likeIndex, 1)
+            : newPost.likes?.push({ userId });
+
+          queryClient.setQueryData(
+            [queryKeys.POST, queryKeys.GET_POSTS, postId],
+            newPost
+          );
+
+          return { previousPosts, newPost };
+        }
+      }
+    },
+    onError: (error, postId, context) => {
+      queryClient.setQueryData(
+        [queryKeys.POST, queryKeys.GET_POSTS, context?.previousPosts.id], // 쿼리키 수정
+        context?.previousPosts
+      );
+    },
+    onSettled: (data, error, postId) => {
       queryClient.invalidateQueries({
         queryKey: [queryKeys.POST, queryKeys.GET_POSTS, postId],
       });
-    },
-    onError: (error) => {
-      console.error("Error liking post:", error);
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
+      });
     },
   });
 }
